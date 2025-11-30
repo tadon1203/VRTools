2980914379739fb38fcdd1b15ef0317c4c3b02b5
tadon1203 <nosashuumaiepic@gmail.com>
2025-11-30 14:04:08 +0900

    feat: inputmanager now handles mouse input

--- src/Input/InputManager.cpp
+++ src/Input/InputManager.cpp
@@ -1,5 +1,7 @@
 #include "InputManager.hpp"
 
+#include <windowsx.h> // GET_X_LPARAM, GET_Y_LPARAM
+
 #include "Hooking/WndProcHook.hpp"
 
 InputManager& InputManager::instance() {
@@ -13,6 +15,37 @@ void InputManager::initialize() {
     });
 }
 
+void InputManager::update() {
+    for (const auto& [vkCode, callback] : m_keybinds) {
+        if (wasKeyPressed(vkCode)) {
+            if (callback) {
+                callback();
+            }
+        }
+    }
+    m_scrollDelta   = 0;
+    m_prevKeyStates = m_keyStates;
+}
+
+// --- Helper ---
+int InputManager::mouseButtonToVk(MouseButton button) {
+    switch (button) {
+    case MouseButton::Left:
+        return VK_LBUTTON;
+    case MouseButton::Right:
+        return VK_RBUTTON;
+    case MouseButton::Middle:
+        return VK_MBUTTON;
+    case MouseButton::Side1:
+        return VK_XBUTTON1;
+    case MouseButton::Side2:
+        return VK_XBUTTON2;
+    default:
+        return 0;
+    }
+}
+
+// --- Keyboard Queries ---
 bool InputManager::isKeyDown(int vkCode) const {
     if (vkCode < 0 || vkCode >= 256) {
         return false;
@@ -27,17 +60,22 @@ bool InputManager::wasKeyPressed(int vkCode) const {
     return m_keyStates[vkCode] && !m_prevKeyStates[vkCode];
 }
 
-void InputManager::registerKeybind(int vkCode, const KeybindCallback& callback) { m_keybinds[vkCode] = callback; }
-
-void InputManager::update() {
-    for (const auto& pair : m_keybinds) {
-        if (wasKeyPressed(pair.first)) {
-            pair.second();
-        }
+bool InputManager::wasKeyReleased(int vkCode) const {
+    if (vkCode < 0 || vkCode >= 256) {
+        return false;
     }
-
-    m_prevKeyStates = m_keyStates;
+    return !m_keyStates[vkCode] && m_prevKeyStates[vkCode];
 }
+
+bool InputManager::isMouseButtonDown(MouseButton button) const { return isKeyDown(mouseButtonToVk(button)); }
+
+bool InputManager::wasMouseButtonPressed(MouseButton button) const { return wasKeyPressed(mouseButtonToVk(button)); }
+
+MousePos InputManager::getMousePosition() const { return m_mousePos; }
+
+int InputManager::getScrollDelta() const { return m_scrollDelta; }
+
+void InputManager::registerKeybind(int vkCode, const KeybindCallback& callback) { m_keybinds[vkCode] = callback; }
 
 bool InputManager::onWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
@@ -53,23 +91,58 @@ bool InputManager::onWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             m_keyStates[wParam] = false;
         }
         break;
+
     case WM_LBUTTONDOWN:
         m_keyStates[VK_LBUTTON] = true;
         break;
     case WM_LBUTTONUP:
         m_keyStates[VK_LBUTTON] = false;
         break;
+
     case WM_RBUTTONDOWN:
         m_keyStates[VK_RBUTTON] = true;
         break;
     case WM_RBUTTONUP:
         m_keyStates[VK_RBUTTON] = false;
         break;
+
     case WM_MBUTTONDOWN:
         m_keyStates[VK_MBUTTON] = true;
         break;
     case WM_MBUTTONUP:
         m_keyStates[VK_MBUTTON] = false;
+        break;
+
+    case WM_XBUTTONDOWN:
+        {
+            int xButton = GET_XBUTTON_WPARAM(wParam);
+            if (xButton == XBUTTON1) {
+                m_keyStates[VK_XBUTTON1] = true;
+            }
+            if (xButton == XBUTTON2) {
+                m_keyStates[VK_XBUTTON2] = true;
+            }
+            break;
+        }
+    case WM_XBUTTONUP:
+        {
+            int xButton = GET_XBUTTON_WPARAM(wParam);
+            if (xButton == XBUTTON1) {
+                m_keyStates[VK_XBUTTON1] = false;
+            }
+            if (xButton == XBUTTON2) {
+                m_keyStates[VK_XBUTTON2] = false;
+            }
+            break;
+        }
+
+    case WM_MOUSEMOVE:
+        m_mousePos.x = GET_X_LPARAM(lParam);
+        m_mousePos.y = GET_Y_LPARAM(lParam);
+        break;
+
+    case WM_MOUSEWHEEL:
+        m_scrollDelta += GET_WHEEL_DELTA_WPARAM(wParam) / WHEEL_DELTA;
         break;
     }
     return false;
--- src/Input/InputManager.hpp
+++ src/Input/InputManager.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <Windows.h>
+
 #include <array>
 #include <functional>
 #include <unordered_map>
 
-#include <Windows.h>
+struct MousePos {
+    long x;
+    long y;
+};
+
+enum class MouseButton { Left, Right, Middle, Side1, Side2 };
 
 using KeybindCallback = std::function<void()>;
 
@@ -16,13 +23,19 @@ public:
     InputManager& operator=(const InputManager&) = delete;
 
     void initialize();
+    void update();
 
-    bool isKeyDown(int vkCode) const;
-    bool wasKeyPressed(int vkCode) const;
+    [[nodiscard]] bool isKeyDown(int vkCode) const;
+    [[nodiscard]] bool wasKeyPressed(int vkCode) const;
+    [[nodiscard]] bool wasKeyReleased(int vkCode) const;
+
+    [[nodiscard]] bool isMouseButtonDown(MouseButton button) const;
+    [[nodiscard]] bool wasMouseButtonPressed(MouseButton button) const;
+
+    [[nodiscard]] MousePos getMousePosition() const;
+    [[nodiscard]] int getScrollDelta() const;
 
     void registerKeybind(int vkCode, const KeybindCallback& callback);
-
-    void update();
 
 private:
     InputManager()  = default;
@@ -30,7 +43,12 @@ private:
 
     bool onWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+    [[nodiscard]] static int mouseButtonToVk(MouseButton button);
+
     std::array<bool, 256> m_keyStates{};
     std::array<bool, 256> m_prevKeyStates{};
     std::unordered_map<int, KeybindCallback> m_keybinds;
+
+    MousePos m_mousePos{ 0, 0 };
+    int m_scrollDelta = 0;
 };
